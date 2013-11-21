f704fd7fcf2a8ca31bb5df04ed6ff71f2f1ffee7
Christian Borys <christianborys@gmail.com>
2013-11-21 15:47:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roommate::Application.config.secret_key_base = '57a02422ae6f0f8a8b89efa574fee8b0a9ceeba065636c1671de13273ff859baba5e824b3603e5d815fdc3234e4f358cd5c7be855ca726658c3d7b4684be880c'
