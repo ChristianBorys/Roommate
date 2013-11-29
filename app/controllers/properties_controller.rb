3f8b7752997504c76fddb63f22a7df897c97df57
John Zdanis <Zdanis.john@gmail.com>
2013-11-29 01:35:20 -0500

    added property#index method and view

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,28 @@
 class PropertiesController < ApplicationController
+
+  def index
+    @properties = Property.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:rent, :size, :address, :latitude, :longitude)
+  end
 end
